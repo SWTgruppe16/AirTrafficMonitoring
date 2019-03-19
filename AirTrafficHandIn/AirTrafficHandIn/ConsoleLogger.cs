a221b42327baecbda1091886dadda7a9b325120a
carolinekdl <caroline.kaagaard.dahl.laursen@post.au.dk>
2019-03-19 12:07:36 +0100

    Adding Track Data (Tracks Class)

--- AirTrafficHandIn/AirTrafficHandIn/ConsoleLogger.cs
+++ AirTrafficHandIn/AirTrafficHandIn/ConsoleLogger.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AirTrafficHandIn
 {
-    class Logger
+    class ConsoleLogger
     {
     }
 }
---
+++ AirTrafficHandIn/AirTrafficHandIn/Tracks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficHandIn
+{
+    class Tracks
+    {
+        public string TagId { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Altitude { get; set; }
+        public double Velocity { get; set; }
+        public double CompassCourse { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
