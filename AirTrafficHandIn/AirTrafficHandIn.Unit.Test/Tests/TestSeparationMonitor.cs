85e5fd891ae93519ca713fa1d0363fd827f71030
henriettepilegaard <201507843@post.au.dk>
2019-04-20 14:35:01 +0200

    Creating new test class, TestSeparationMonitor

---
+++ AirTrafficHandIn/AirTrafficHandIn.Unit.Test/Tests/TestSeparationMonitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AirTrafficHandIn.Unit.Test.Tests
+{
+    class TestSeparationMonitor
+    {
+        private SeparationMonitor _uut;
+        private CurrentConditions _currentConditions;
+        private NewCondition _newCondition;
+        private List<SeparationMonitor> separationMonitors;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            _uut = new SeparationMonitor();
+
+        }
+        
+    }
+}
