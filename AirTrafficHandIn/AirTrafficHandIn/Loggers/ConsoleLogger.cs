b09f32755dd2998c10c74be2ccdeb445ab98ea4f
carolinekdl <caroline.kaagaard.dahl.laursen@post.au.dk>
2019-04-23 13:26:22 +0200

    exclude classes from coverage

--- AirTrafficHandIn/AirTrafficHandIn/Loggers/ConsoleLogger.cs
+++ AirTrafficHandIn/AirTrafficHandIn/Loggers/ConsoleLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using AirTrafficHandIn.Interfaces;
 
 namespace AirTrafficHandIn
 {
+    [ExcludeFromCodeCoverage]
     public class ConsoleLogger : ILogger
     {
         private string trackEnteredString = "";
